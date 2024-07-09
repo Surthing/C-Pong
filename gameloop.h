b1ef4b70197f37150ba2b15b27ecdbf72e4cdb05
Surthing <bartoszfrederik@gmail.com>
2024-07-09 14:40:14 +0200

    Update gameloop.h

--- gameloop.h
+++ gameloop.h
@@ -1,7 +1,7 @@
 #ifndef GAMELOOP_H
 #define GAMELOOP_H
 
-
+//structs für pointer und variabel ändern
 typedef struct {
     int x, y;
     bool is_alternate;
@@ -11,7 +11,7 @@ typedef struct {
     int x, y, dx, dy;
     int old_x, old_y; 
 } Ball;
-
+//alle funktionen aufzählen
 void render_frame();
 bool update_state();
 void game_loop();
@@ -25,6 +25,9 @@ void cleanup_ncurses();
 void init_game_canvas();
 void init_ncurses();
 void init_sprites();
-
+void init_intro_canvas();
+void render_intro();
+void render_game();
+void disable_sprites();
 #endif // GAMELOOP_H
 
